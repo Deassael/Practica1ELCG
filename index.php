5892fa83fb1742ae0630d05552fd1eace22792a5
Eick Leonardo Cruz Gil <erickdeathsoul@outlook.com>
2024-08-13 19:58:07 -0600

    Creacion de index con estructura html

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Erick Leonardo Cruz Gil </title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css"/>
+        <script src = "js/jquery-3.7.1.min.js"></script>
+        <script src = "js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
+
